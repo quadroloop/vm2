20d0edf148c5b11b76e3c9d819506873ee79b142
quadroloop <nplixel@gmail.com>
2018-08-27 07:54:32 -0700

    initial commit, fileset and UI

---
+++ UI/index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<meta name="theme-color" content="#000" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+<link rel="stylesheet" href="./css/bootstrap4.css">
+<link rel="stylesheet" href="./css/vm2.css">
+</head>
+<body>
+	<nav class="navbar navbar-light nav-dark">
+  <a class="navbar-brand" href="#">
+    <img src="./img/v2.gif" width="50" height="50" class="d-inline-block align-top" alt="">
+    <span class="logo-text"> vm2</span>
+  </a>
+  <button class="navbar-toggler d-none" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon" style="color:#fff !important"></span>
+  </button>
+      
+      <div class="right">
+
+      	   <button class="btn btn-primary mr-2">Assembly</button>
+      	    <button class="btn btn-info mr-2">Dissassembly</button>
+      	    <button class="btn btn-warning mr-2">Boot</button>
+
+      </div>
+</nav>
+  <div class="mx-3 mt-2 pt-2">
+	 <div class="row">
+
+	 	  <div class="col-md-4 border-right">
+	 	  	  <img src="./img/mb1.png" class="img-fluid">
+	 	  </div>
+
+          <div class="col-md-3 border-right">
+              <ul class="list-group">
+				  <li class="list-group-item">CPU / Microprocessor</li>
+				  <li class="list-group-item">RAM / Radom Access Memory</li>
+				  <li class="list-group-item">ROM / Read Only Memory</li>
+				  <li class="list-group-item">Power Input</li>
+				  <li class="list-group-item">Vestibulum at eros</li>
+				</ul>
+          </div>
+
+	 </div>
+  </div>
+</body>
+</html>
