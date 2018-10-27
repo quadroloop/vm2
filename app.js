44e149e4e44c01b8d79d66aac9759909802410b6
quadroloop <nplixel@gmail.com>
2018-10-27 14:35:23 +0800

    last, update. Project Finished

--- app.js
+++ app.js
@@ -69,6 +69,43 @@ io.sockets.on('connection', function(socket) {
 
 });
 
+var ip;
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      // console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      ip=iface.address;
+    }
+    ++alias;
+  });
+});
 // http.listen(app.get('port'), function() {
 //   console.log('listening on *:' + app.get('port'));
 // });
+console.log(`
+  ===========================================
+                 Instructions:
+  ===========================================
+   Instruct your students to connect
+   to your wireless network, and go to this
+   link using their browser:
+
+   http://${ip}:5000
+
+   after this they must be able to access the 
+   vm2 web application.
+  `);
